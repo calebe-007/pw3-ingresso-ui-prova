a7e372f568364f2e96a6c10f1aa7e2423b7eec70
etechas <etechas>
2026-06-23 13:59:08 -0300

    Atualização

---
+++ src/app/core/services/filme-service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Filme } from '../model/filme';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilmeService {
+  private apiUrl = 'http://localhost:8090/filmes';
+  private http = inject(HttpClient);
+
+  findCartaz(): Observable<Filme[]>{
+    return this.http.get<Filme[]>(`${this.apiUrl}/em-cartaz`);
+  }
+  
+  //findById(id: Number): Observable<Filme>{
+  //  return null;
+  //}
+
+  //findSessoesByFilmeId(id: Number): Observable<Sessao[]>{
+  //  return null;
+  //}
+
+}
